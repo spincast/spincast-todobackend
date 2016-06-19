f56cd199e3a53d78c893b9ae044264282b627135
electrotype <electrotype@gmail.com>
2016-06-19 14:03:42 -0400

    Update to Spincast 0.9.21

--- spincast-todobackend-inmemory/src/test/java/org/spincast/todobackend/tests/AppIntegrationTestBase.java
+++ spincast-todobackend-inmemory/src/test/java/org/spincast/todobackend/tests/AppIntegrationTestBase.java
@@ -8,6 +8,7 @@ import org.spincast.core.json.IJsonArray;
 import org.spincast.core.json.IJsonManager;
 import org.spincast.core.utils.ContentTypeDefaults;
 import org.spincast.core.utils.SpincastStatics;
+import org.spincast.core.websocket.IDefaultWebsocketContext;
 import org.spincast.plugins.httpclient.IHttpResponse;
 import org.spincast.shaded.org.apache.http.HttpStatus;
 import org.spincast.testing.core.SpincastIntegrationTestBase;
@@ -20,7 +21,8 @@ import com.google.inject.Injector;
  * Integration test base class specifically made for 
  * our application.
  */
-public abstract class AppIntegrationTestBase extends SpincastIntegrationTestBase<IDefaultRequestContext> {
+public abstract class AppIntegrationTestBase extends
+                                             SpincastIntegrationTestBase<IDefaultRequestContext, IDefaultWebsocketContext> {
 
     @Inject
     protected IJsonManager jsonManager;
@@ -31,7 +33,18 @@ public abstract class AppIntegrationTestBase extends SpincastIntegrationTestBase
      */
     @Override
     protected Injector createInjector() {
-        return App.createApp(null, null);
+
+        //==========================================
+        // Make sure you add the Guice Module returned by
+        // getTestOverridingModule(...) as an overriding Module!
+        // You can also modify it, before adding it.
+        //==========================================
+        return App.createApp(getMainArgs(),
+                             getTestOverridingModule(IDefaultRequestContext.class, IDefaultWebsocketContext.class));
+    }
+
+    protected String[] getMainArgs() {
+        return null;
     }
 
     @Override
